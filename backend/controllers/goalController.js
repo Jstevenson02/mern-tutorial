eabeb51e910b878d48b39f419b3a3d7286406a38
Jacob Stevenson <jacobryanstevenson@gmail.com>
2022-02-09 19:59:40 -0600

    REST API for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,54 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = asnycHandler(async (req, res) => {
-    res.send({ message: 'Get goals' })
+const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find()
+    res.send(goals)
 })
 
 // @desc    Set goals
 // @route   POST /api/goals
 // @access  Private
-const setGoal = asnycHandler(async(req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field!')
     }
-    res.send({ message: 'Set goals' })
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.send(goal)
 })
 
 // @desc    Update goal
 // @route   PUT /api/goals
 // @access  Private
-const updateGoal = asnycHandler(async(req, res) => {
-    res.send({ message: `Update goal ${req.params.id}` })
+const updateGoal = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true },)
+    res.send(updatedGoal)
 })
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = asnycHandler(async(req, res) => {
-    res.send({ message: `Delete goal ${req.params.id}` })
+const deleteGoal = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.remove()
+
+    res.send({ id: req.params.id })
 })
 
 module.exports = {
