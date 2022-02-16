b3720a655eaea6b7493be79bce6e6bd88edb2b18
Jacob <jacobryanstevenson@gmail.com>
2022-02-15 21:55:32 -0600

    frontend update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,54 @@
 import { useState, useEffect } from 'react';
 import { FaSign, FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner';
+import { login, reset } from '../features/auth/authSlice'
+
 function Login() {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
 
-  const { email, password} = formData;
+  const { email, password } = formData;
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset)
+
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }))
-   }
+  }
+
+  if(isLoading) {
+    return <Spinner/>
+  }
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+    dispatch(login(userData))
   }
 
   return <>
@@ -42,10 +74,10 @@ function Login() {
         </div>
         <div className='form-group'>
           <input
-            type='text'
+            type='password'
             className='form-control'
             id='password'
-            name='passord'
+            name='password'
             value={password}
             placeholder='Enter your password'
             onChange={onChange}
