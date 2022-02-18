00db8d724ff275ce3fe852258421fe846c0840ed
Jacob <jacobryanstevenson@gmail.com>
2022-02-18 16:45:28 -0600

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
-const path = require('path')
 const express = require('express');
 const colors = require('colors')
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db');
-const res = require('express/lib/response');
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 connectDB()
 
@@ -32,4 +30,4 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server started at port ${port}`));
+app.listen(port, () => console.log(`Server started at port ${port}`))
